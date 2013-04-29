e7f98b1a099253e944950f1507a3d28e009dafff
nyux <emilia.emerald@gmail.com>
2013-04-29 14:13:53 -0600

    put functions at end of the file, removed vba alias

--- .aliases.bash
+++ .aliases.bash
@@ -1,12 +1,16 @@
 alias reload='source $HOME/.bashrc'
 
-# showa: to remind yourself of an alias (given some part of it)
-showa () { /usr/bin/grep -i -a1 $@ ~/.aliases.bash | grep -v '^\s*$' ; }
-
 alias gping='ping -c 5 8.8.8.8'
 alias xping='ping -c 5 www.xkcd.com'
 
 alias untar='tar -zxvf'
+
+# depends on openssh
+alias irc="ssh emilia@zecora.aiiane.com"
+alias moons="ssh eazure@moons.cs.unm.edu"
+
+# showa: to remind yourself of an alias (given some part of it)
+showa () { /usr/bin/grep -i -a1 $@ ~/.aliases.bash | grep -v '^\s*$' ; }
 
 # performs 'ls' after 'cd' if directory change is successful.
 cdls() {
@@ -36,9 +40,3 @@ man() {
     LESS_TERMCAP_us=$(printf "\e[1;32m") \
       man "$@"
 }
-
-alias vba="VisualBoyAdvance"
-
-alias irc="ssh emilia@zecora.aiiane.com"
-alias moons="ssh eazure@moons.cs.unm.edu"
-
