3a5526d9ae6335a80a93d75ab75f945d8c099349
nyux <emilia.emerald@gmail.com>
2013-05-08 13:08:57 -0600

    brightness.py -i|d requires sudo; separated functionality in script

--- .i3/brightness.py
+++ .i3/brightness.py
@@ -11,29 +11,13 @@ min_brightness = 0
 parser = argparse.ArgumentParser(description="Manipulate keyboard brightness.", prog="kbd_brightness")
 group = parser.add_mutually_exclusive_group()
 
-parser.add_argument('-k', '--keyboard', action='store_true')
-group.add_argument('-i', '--increment', action='store_true')
-group.add_argument('-d', '--decrement', action='store_true')
-parser.add_argument('-m', '--monitor', action='store_true')
+parser.add_argument('-k', '--keyboard', action='store_true', help="get keyboard brightness")
+parser.add_argument('-m', '--monitor', action='store_true', help="get monitor brightness")
+group.add_argument('-i', '--increment', action='store_true', help="increment keyboard brightness; needs sudo")
+group.add_argument('-d', '--decrement', action='store_true', help="decrement keyboard brightness; needs sudo")
+
 
 args = parser.parse_args()
-
-with open(brightness_control, 'r+') as brightness:
-
-	value = brightness.read()
-	value = int(value)
-
-	if args.keyboard:
-		print(value)
-
-	if args.increment and value < max_brightness:
-		value += 1
-		value = str(value)
-		brightness.write(value)
-	elif args.decrement and value > min_brightness:
-		value -= 1
-		value = str(value)
-		brightness.write(value)
 
 if args.monitor:
 	command = Popen(["xbacklight"], stdout=PIPE)
@@ -41,3 +25,22 @@ if args.monitor:
 	output = float(output)
 
 	print('{0:.2f}'.format(output))
+elif args.keyboard:
+	with open(brightness_control, 'r') as brightness:
+
+		value = brightness.read().strip()
+		print(value)
+elif args.increment or args.decrement:
+	with open(brightness_control, 'r+') as brightness:
+
+		value = brightness.read()
+		value = int(value)
+
+		if args.increment and value < max_brightness:
+			value += 1
+			value = str(value)
+			brightness.write(value)
+		elif args.decrement and value > min_brightness:
+			value -= 1
+			value = str(value)
+			brightness.write(value)
