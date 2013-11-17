b89de9be07482ac07325d72eae878c657e43aca6
nyux <emilia.emerald@gmail.com>
2013-11-17 12:31:47 -0700

    fixed indentation

--- .i3/study.c
+++ .i3/study.c
@@ -4,17 +4,17 @@
 int main(void) {
 	
     /* Things I wanna do. Changes with time, of course. */
-	char *topics[] = {"novel", "drawing", "assembly", "short stories",
+    char *topics[] = {"novel", "drawing", "assembly", "short stories",
         "read fiction", "go", "read non-fiction", "perl", "irc bot",
-		"calc", "discrete maths", "relax", "real analysis", "haskell"};
-	int num_of_topics = sizeof(topics)/sizeof(char *);
+        "calc", "discrete maths", "relax", "real analysis", "haskell"};
+    int num_of_topics = sizeof(topics)/sizeof(char *);
 
-	struct tm *today;
-	time_t now = time(NULL);
+    struct tm *today;
+    time_t now = time(NULL);
 
-	localtime_r(&now, today);
+    localtime_r(&now, today);
 
-	printf("suggested: %s\n", topics[today->tm_yday % num_of_topics]);
+    printf("suggested: %s\n", topics[today->tm_yday % num_of_topics]);
 
     return 0;
 }
