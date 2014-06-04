11de56c34cb82a315735b575ed40d7b45481fd11
nyux <emilia.emerald@gmail.com>
2014-06-04 12:26:20 -0600

    actually allocated memory for the today struct (*facepalm*), added call to tzset (recommended by the ctime man page for portability), added reminder to pay rent the last few days of the month

--- .i3/study.c
+++ .i3/study.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 int main(void) {
 	
+    struct tm *today = malloc(sizeof(struct tm));
+    time_t now = time(NULL);
+    tzset();
+    localtime_r(&now, today);
+
     /* Things I wanna do. Changes with time, of course. */
     char *topics[] = {"novel", "drawing", "assembly", "short stories",
         "read fiction", "go", "read non-fiction", "perl", "irc bot",
         "calc", "discrete maths", "relax", "real analysis", "haskell"};
     int num_of_topics = sizeof(topics)/sizeof(char *);
 
-    struct tm *today;
-    time_t now = time(NULL);
+    printf("suggested: %s", topics[today->tm_yday % num_of_topics]);
 
-    localtime_r(&now, today);
+    if (today->tm_mday >= 23) {
+        printf("; pay rent!");
+    }
 
-    printf("suggested: %s\n", topics[today->tm_yday % num_of_topics]);
+    putchar('\n');
 
     return 0;
 }
